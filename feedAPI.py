b588b2988c6b5396421a593b7f359cda70ad0ef8
anders <anders@webbsallad.se>
2025-04-14 16:11:05 +0200

    Klart tror jag

---
+++ feedAPI.py
@@ -0,0 +1,18 @@
+import requests
+
+def feed_it(artist,date,time,link, stage_comment):
+    # Utvecklingsurl. OBS place_id och organizer_id måste ändras beroende på miljö:
+    # place_id för utvecklingsmiljön : 1
+    #org_id för utvecklingsmiljön: 5
+    #place_ id för skarpa: 1
+    #org_id för skrapa: 83
+    #url = 'http://localhost/api/v1/events'
+    #Nyckel utvecklingsmiljön: 2|MDybuSA2L23uICjpYchwoqF07enRpwvuuiYD11Y32f517b7a
+    #Nyckel skarpa: 2|CYXAKvuNNEnSlAiy2BgvkPaimuIwXGVMZDMT37M69b25751f
+    #url = 'http://localhost/api/v1/events'
+    url = 'https://jazztider.webbsallad.se/api/v1/events'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization':  "Bearer " + '2|CYXAKvuNNEnSlAiy2BgvkPaimuIwXGVMZDMT37M69b25751f', 'accept': 'application/json'}
+    data = {'name': artist, 'place_id': '1', 'organizer_id': '83', 'day': date,'timeofday': time, 'link': link, 'comment': 'Scen: ' + stage_comment}
+    post_req = requests.post(url,headers=headers, data=data)
+    if post_req.status_code != 200:
+        print("APIET HAR INTE TAGIT EMOT POSTEN!")
--- main.py
+++ main.py
@@ -2,31 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+from feedAPI import feed_it
+
 response = requests.get("https://stockholmjazz.se/")
 festival_page = response.text
 
 soup = BeautifulSoup(festival_page, "html.parser")
-print(soup)
-
-'''
-artists_select = soup.find("select", class_="sort__select")
-artists_options = artists_select.find_all("option")
-artists_options.pop(0)
-option_values = []
-
-for a_o in artists_options:
-    option_values.append(int(a_o["value"]))
+#Gör en lista med konserter
+specific_class = 'thumb--program'
+event_list = soup.find_all('li',class_=specific_class)
+#lista att ladda upp till valuefile
+add_to_jsonlist = []
 
 with open('valuefile.json', 'r') as f:
     old_values_list = json.load(f)
-
-print('\nNya konserter:')
-for o in option_values:
-    if o not in old_values_list:
-        for art_opt in artists_options:
-            if art_opt['value'] == str(o):
-                print(art_opt.get_text())
+    for event in event_list:
+        event = str(event)
+        item_soup = BeautifulSoup(event, "html.parser")
+        # Hitta 'li'
+        li_tag = item_soup.find('li')
+        # Värdet av li-attributet 'data-artst'
+        artst_value = li_tag['data-artst']
+        if int(artst_value) not in old_values_list:
+            artist = item_soup.h2.getText()
+            artist = artist.strip()
+            date = li_tag['data-date']
+            time = li_tag['data-time']
+            link_tags = item_soup.find_all('a')
+            link = link_tags[0]['href']
+            stage_comment = link_tags[2].getText()
+            add_to_jsonlist.append(int(artst_value))
+            print(artist)
+            print(date)
+            print(time)
+            print(link)
+            print(stage_comment)
+            print('----------')
+            if add_to_jsonlist:
+                user_input = input("Mata in posten? j/n:\n")
+                if user_input == "j":
+                    feed_it(artist,date,time,link, stage_comment)
 
 with open('valuefile.json', 'w') as f:
-    json.dump(option_values,f)
-'''
+    if add_to_jsonlist:
+        jsonlist = old_values_list + add_to_jsonlist
+        json.dump(jsonlist,f)
+        print("Sparat till Valuefile")
+    else:
+        json.dump(old_values_list, f)
+        print("Inga nya konserter!")
