1e26d7a6a22a1d312049d9815d2678a75ebd465b
jazzankan <shasfn@gmail.com>
2025-04-14 16:30:27 +0200

    Update feedAPI.py

--- feedAPI.py
+++ feedAPI.py
@@ -11,7 +11,7 @@ def feed_it(artist,date,time,link, stage_comment):
     #Nyckel skarpa: 2|CYXAKvuNNEnSlAiy2BgvkPaimuIwXGVMZDMT37M69b25751f
     #url = 'http://localhost/api/v1/events'
     url = 'https://jazztider.webbsallad.se/api/v1/events'
-    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization':  "Bearer " + '2|CYXAKvuNNEnSlAiy2BgvkPaimuIwXGVMZDMT37M69b25751f', 'accept': 'application/json'}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization':  "Bearer " + 'MY_KEY', 'accept': 'application/json'}
     data = {'name': artist, 'place_id': '1', 'organizer_id': '83', 'day': date,'timeofday': time, 'link': link, 'comment': 'Scen: ' + stage_comment}
     post_req = requests.post(url,headers=headers, data=data)
     if post_req.status_code != 200:
